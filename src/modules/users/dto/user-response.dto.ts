cf6de305c5f7d49d258ef464b7431d7de122147b
rumahkodingku <rumahkodingku45@gmail.com>
2026-01-23 10:11:28 +0700

    refactor: add new field in user response dto

--- src/modules/users/dto/user-response.dto.ts
+++ src/modules/users/dto/user-response.dto.ts
@@ -1,24 +1,42 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Expose } from 'class-transformer';
+import { UserStatus } from 'src/common/enums/user-status.enum';
 import { UserEntity } from '../entities/user.entity';
 
 export class UserResponseDto {
     @ApiProperty({
         example: '550e8400-e29b-41d4-a716-446655440000',
         description: 'The unique identifier of the user (UUID)',
+        nullable: false,
+        type: String,
     })
     @Expose()
     id: string;
 
-    @ApiProperty({ example: 'johndoe', description: 'The unique username of the user' })
+    @ApiProperty({
+        example: 'johndoe',
+        description: 'The unique username of the user',
+        nullable: false,
+        type: String,
+    })
     @Expose()
     username: string;
 
-    @ApiProperty({ example: 'John Doe', description: 'The full name of the user' })
+    @ApiProperty({
+        example: 'John Doe',
+        description: 'The full name of the user',
+        nullable: false,
+        type: String,
+    })
     @Expose()
     fullName: string;
 
-    @ApiProperty({ example: 'john@example.com', description: 'The registered email address' })
+    @ApiProperty({
+        example: 'john@example.com',
+        description: 'The registered email address',
+        nullable: false,
+        type: String,
+    })
     @Expose()
     email: string;
 
@@ -26,6 +44,7 @@ export class UserResponseDto {
         example: 'https://cdn.example.com/avatars/user1.jpg',
         description: 'URL to the user profile picture',
         nullable: true,
+        type: String,
     })
     @Expose()
     avatarUrl: string;
@@ -34,6 +53,7 @@ export class UserResponseDto {
         example: 'Software Engineer based in Jakarta.',
         description: 'User short biography',
         nullable: true,
+        type: String,
     })
     @Expose()
     bio: string;
@@ -48,9 +68,33 @@ export class UserResponseDto {
     @Expose()
     emailVerifiedAt: Date | null;
 
-    @ApiProperty({ example: true, description: 'Verification status of the account' })
+    @ApiProperty({
+        example: true,
+        description: 'Verification status of the account',
+        type: Boolean,
+        default: false,
+    })
     @Expose()
     isVerified: boolean;
+
+    @ApiProperty({
+        example: UserStatus.ONLINE,
+        description: 'User status ',
+        enum: UserStatus,
+        default: UserStatus.ONLINE,
+    })
+    @Expose()
+    status: UserStatus;
+
+    @ApiProperty({
+        example: '2026-01-01T10:00:00.000Z',
+        description: 'Last seen timestamp',
+        nullable: true,
+        type: String,
+        format: 'date-time',
+    })
+    @Expose()
+    lastSeenAt: Date;
 
     @ApiProperty({
         example: '2026-01-01T10:00:00.000Z',
